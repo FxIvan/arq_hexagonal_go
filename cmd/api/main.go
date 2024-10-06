27a86d99e86552301142d4c0de8f4cc84cd2e637
fxivaninfobae <ialmendra@infobae.com>
2024-10-06 20:53:48 -0300

    conectando con DB | Haciendo Ping | Conectando a una collection e insertando

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,34 +1,65 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Player struct{
-	Name			string		`json:"name"	binding:"required"`
-	Age				int			`json:"age"		binding:"required"`
-	CreationTime	time.Time	`json:"creation_time"`
+type Player struct {
+	Name         string    `json:"name"	binding:"required"`
+	Age          int       `json:"age"		binding:"required"`
+	CreationTime time.Time `json:"creation_time"`
 }
 
-func main(){
-	
+func main() {
+
 	err := godotenv.Load()
-	if err != nil{
+	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	ginEngine := gin.Default()
 
-	ginEngine.POST("/players", func(c *gin.Context){
+	ginEngine.POST("/players", func(c *gin.Context) {
 		var player Player
-		if err := c.BindJSON(&player); err != nil{
+		if err := c.BindJSON(&player); err != nil {
 			c.JSON(400, gin.H{"error": err.Error()})
 			return
 		}
+		player.CreationTime = time.Now().UTC()
+
+		// Connection to the DB, if the connection to the DB takes longer than 10 seconds
+		// then the connection is cancelled
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// Connect to the MongoDB server
+		client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Call the ping method yo verify that the connection has been stablished successfully
+		err = client.Ping(ctx, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Conectamos a una base de datos("go-l") y obtenemos una coleccion("players")
+		collection := client.Database("tangowallet").Collection("players")
+		insertResult, err := collection.InsertOne(ctx, player)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(200, gin.H{"player_id": insertResult.InsertedID})
 	})
 
 	log.Fatal(ginEngine.Run(":8001"))
--- readme.md
+++ readme.md
@@ -5,3 +5,12 @@ go mod init github.com/greetings
 Package for install:
 1. go get github.com/joho/godotenv
 2. go get -u github.com/gin-gonic/gin
+3. go get go.mongodb.org/mongo-driver/mongo
+
+
+Buenas practicas:
+Poner el tiempo en UTC
+```
+player.CreationTime = time.Now().UTC()
+```
+
