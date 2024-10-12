35aa6aef4b1dd97109ff638afc0beb2242562eed
fxivaninfobae <ialmendra@infobae.com>
2024-10-12 20:35:02 -0300

    separando en funcion la conexion de la db

--- internal/services/player/create.go
+++ internal/services/player/create.go
@@ -12,11 +12,30 @@ import (
 	"github.com/arq_hexagonal/internal/domain"
 )
 
+//////////////////////////////////////// Consumision de servicio //////////////////////////////////////
+
 func (s Service) Create(player domain.Player) (id interface{}, err error) {
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
-	//////////////////////////////////////// Consumision de servicio //////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
+	/**
+	* Responsabilidades que tiene que tener un servicio:
+	* Set creation time
+	* Save to repo
+	* Responder con el id del recurso creado
+	**/
+
+	// Este es el primer paso que es -> Set creation time
 	player.CreationTime = time.Now().UTC()
+	// ================================ REPO
+	insertResult, err := Save(player)
+	if err != nil {
+		return nil, err
+	}
+	// ================================ REPO
+	// Este es el tercer paso -> Responder con el id del recurso creado
+	return insertResult, nil
+}
+
+func Save(player domain.Player) (id interface{}, err error) {
+	// ================================ REPO
 	// Connection to the DB, if the connection to the DB takes longer than 10 seconds
 	// then the connection is cancelled
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -26,12 +45,14 @@ func (s Service) Create(player domain.Player) (id interface{}, err error) {
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
 
 	// Call the ping method yo verify that the connection has been stablished successfully
 	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
 
 	// Conectamos a una base de datos("go-l") y obtenemos una coleccion("players")
@@ -39,9 +60,9 @@ func (s Service) Create(player domain.Player) (id interface{}, err error) {
 	insertResult, err := collection.InsertOne(ctx, player)
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
-	return insertResult, nil
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////FIN//////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	return insertResult.InsertedID, nil
+	// ================================ REPO
 }
